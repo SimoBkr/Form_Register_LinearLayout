36f16603d5afd1c1d35a39a6c048aff96b437a6e
SimoBkr <simobkr.sb@gmail.com>
2020-03-16 22:42:20 +0100

    Initial commit

---
+++ app/src/main/java/com/simo/firsttp/Leaner_Activity_Test.java
@@ -0,0 +1,14 @@
+package com.simo.firsttp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Leaner_Activity_Test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaner___test);
+    }
+}
